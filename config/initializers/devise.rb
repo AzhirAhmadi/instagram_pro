3005fe32140bbb4c2f8a4ae563f3397e076b7018
AzhirAhmadi <aaahmadi99@gmail.com>
2019-09-02 18:09:03 +0430

    -add twitter auth

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -296,4 +296,8 @@ Devise.setup do |config|
   # When set to false, does not sign a user in automatically after their password is
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
+  API_KEY = "RKXa3Iuxfz3kR0dMOIwdPo9UV"
+  API_SECRET_KEY = "9NERH04wj9J2V4jCoUn2x5jnw7MwcqOmt5RUPiwgvsJJMZ8PhC"
+  config.omniauth :twitter, API_KEY, API_SECRET_KEY
+
 end
