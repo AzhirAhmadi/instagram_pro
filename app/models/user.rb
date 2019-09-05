388f016465c085cf918145dea75bfc1f7d1f6188
AzhirAhmadi <aaahmadi99@gmail.com>
2019-09-05 15:57:44 +0430

    add commenting for post

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ApplicationRecord
 
   has_many :posts, dependent: :destroy
   has_many :likes, dependent: :destroy
+  has_many :comment, dependent: :destroy
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
