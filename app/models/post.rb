ae25ae25cf8832f3e654890b370654e6d3d2e26f
AzhirAhmadi <aaahmadi99@gmail.com>
2019-09-05 13:06:02 +0430

    add liking to project

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,4 +7,8 @@ class Post < ApplicationRecord
   def is_belongs_to? user
     Post.find_by(user_id: user.id, id: id)
   end
+
+  def is_liked user
+    Like.find_by(user_id: user.id, post_id: id)
+  end
 end
