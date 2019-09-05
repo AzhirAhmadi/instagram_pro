4f5948f9fef0330ca68b08dc4addd430b7b20d7d
AzhirAhmadi <aaahmadi99@gmail.com>
2019-09-05 16:10:58 +0430

    add bookmark table to project and run 'rails db:migrate'

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_05_100801) do
+ActiveRecord::Schema.define(version: 2019_09_05_113819) do
+
+  create_table "bookmarks", force: :cascade do |t|
+    t.integer "post_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_bookmarks_on_post_id"
+    t.index ["user_id"], name: "index_bookmarks_on_user_id"
+  end
 
   create_table "comments", force: :cascade do |t|
     t.text "content"
