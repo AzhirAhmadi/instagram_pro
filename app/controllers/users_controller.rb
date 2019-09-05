7de7d05e2af58e1f5b36cc0a68d223d698314282
AzhirAhmadi <aaahmadi99@gmail.com>
2019-09-05 20:17:19 +0430

    -correct some issues in saved post -add user.name to posts in profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,8 +8,9 @@ class UsersController < ApplicationController
     def show
         @user = User.find(params[:id])
         @posts = @user.posts.includes(:photos, :likes, :comments)
-        @saved = Post.joins(:bookmarks).where(user_id: current_user.id).
-            includes(:photos, :likes, :comments) if @user == current_user
+        # @saved = Post.joins(:bookmarks).where(user_id: current_user.id).
+        #     includes(:photos, :likes, :comments) if @user == current_user
+        @saved = Post.joins("INNER JOIN bookmarks ON bookmarks.post_id = posts.id AND bookmarks.user_id = #{current_user.id}")if @user == current_user
     end
 end
       
